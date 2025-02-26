22fff904fe97ad77f824832b0585442629223a91
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-02-26 17:40:31 +0530

    completed shortenerurl proj

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -12,18 +12,32 @@ export async function handleShortenerUrlGenerator(req: Request, res: Response) {
         const shortID = shortid()
 
         await URL.create({
-            shortID:shortID,
-            redirectURL:url,
-            visitHistory:[]
+            shortID: shortID,
+            redirectURL: url,
+            visitHistory: []
 
         })
-        res.status(200).json({ id: shortID})
+        res.status(200).json({ id: shortID })
     } catch {
         res.status(500).json({ message: "Something went wrong" })
     }
 }
 
-export function handleUrlRedirect(req: Request, res: Response) {
-    res.json({ message: "respose from the server" })
+export async function handleUrlRedirect(req: Request, res: Response) {
+    try {
+        const { shortID } = req.params
+        if (!shortID) {
+            res.status(404).json({ message: "Please enter valid short id in the url" })
+            return
+        }
+        const urlData = await URL.findOneAndUpdate({ shortID }, { $push: { visitHistory: {timestamp:Date.now()} } })
+        if (!urlData) {
+            res.status(404).json({ message: "no such data found with that short id" })
+            return
+        }
+        res.redirect(urlData.redirectURL)
+    } catch (error) {
+        res.status(500).json({ message: "Somethings went wrong" })
+    }
 
 }
